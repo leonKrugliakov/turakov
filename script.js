8836997d4c7d783fc1d1afa6132c8e86c6c170fc
leonKrugliakov <leonchikyj@gmail.com>
2017-06-06 22:36:30 -0500

    Almost done, now need only a couple of bug fixes and adding a 'how it works page'

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
   function outputStories(randomSource){
     $.getJSON("https://newsapi.org/v1/articles?source=" + randomSource + "&apiKey=38736fedc4b0460fafc18e3d9b26d00a", function(data){
       var randy = Math.floor(Math.random() * (data.articles.length - 0 + 1)) + 0;
-      $('#output').attr('src', data.articles[0].url);
+      $('#output').attr('src', data.articles[randy].url);
       $('#output').attr('height', "2000px");
     });
   }
 
   $.getJSON("https://newsapi.org/v1/sources", function(data){
-    var array = [0,1,4,5,6,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,26,28,31,33,34,36,40,41,42,44,46,49,50,51,52,57,60,62,64,65,66,67];
-    var randy = array[Math.floor(Math.random() * (42 - 0 + 1)) + 0];
+    var array = [0,1,4,5,8,9,10,11,12,14,17,19,20,26,34,40,42,49,50,51,52,57,60,62];
+    var randy = array[Math.floor(Math.random() * (array.length - 0 + 0)) + 0];
+    console.log(randy);
     newsSource = data.sources[randy].id;
     outputStories(newsSource);
   });
